ea2b6cfc9e5f6fad9297b402424bc557560db434
FredericoCL <fred.lessa@gmail.com>
2018-04-27 00:31:31 -0300

    Estilização simples de alguns elementos da página jogo.html como remoção de bordas, e na página jogo.js criação da função para contagem de balão de acordo com a quantidade definida no escopo do projeto

--- jogo.js
+++ jogo.js
@@ -20,4 +20,29 @@ function iniciaJogo() {
 		tempo_segundos = 30;
 	}
 
+	//Inserindo os Segundos (o contador) no span
+	document.getElementById('cronometro').innerHTML = tempo_segundos;
+
+	//Quantidade de Balões no Cenário do Jogo
+	var qtde_baloes = 10;
+	cria_baloes(qtde_baloes);
+
+	//Imprimir quantidade de balões inteiros
+	document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+
+	//Imprimir quantidade de balões estourados
+	document.getElementById('baloes_estourados').innerHTML = 0;
+
+}
+
+function cria_baloes(qtde_baloes){
+
+	for (var i = 1; i <= qtde_baloes; i++) {
+		var balao = document.createElement("img");
+		balao.src = 'imagens/balao_azul_pequeno.png';
+		balao.style.margin = '10px';
+
+		document.getElementById('cenario').appendChild(balao);
+	}
+
 }
